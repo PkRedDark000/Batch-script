0c12a3ad6e44d7f10676468aaa52e6f8cbd21160
Ramram43 <parasuraman4439@gmail.com>
2025-04-19 20:46:23 +0300

    batch -v.01

--- step by step/input-and-store-variable.bat
+++ step by step/input-and-store-variable.bat
@@ -1,13 +1,16 @@
 @echo off
 
-::input comment
+::input comment {/p}
 set /p name=Enter Your Name :
+set /p age=Enter Your Age :
 set /p pass=Enter Your Password :
+set /p mail=Enter Your Mail address :
+
 
 ::print comment
 echo Your name is : %name%
+echo Your age is : %age%
+echo Your mail is : %mail%
 echo Your Password is : %pass%
-
-
 
 pause
